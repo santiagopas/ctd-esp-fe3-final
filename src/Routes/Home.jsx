e59d65d20767d444910ceead17e09b087c4525a4
santiago pasciullo <santiagopasciullo@gmail.com>
2023-06-28 13:43:47 -0300

    :art: Estilos

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import { ContextGlobal } from '../Components/utils/global.context';
 import Card from '../Components/Card';
 import '../assets/styles/Routes/Home.css'
@@ -10,15 +10,25 @@ const Home = () => {
   const users = state.data;
 
   return (
-    <main className='main-container'>' 
-          <h1>Clinica dental muelita</h1>
-      <section className='hero'>
-      <h2></h2>
+    <main className='main-container'>
+      <section class="hero">
+        <div class="hero-content">
+          <div class="hero-text">
+            <h1 className='animated-text'>Clinica Muelita</h1>
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat aspernatur </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+          {/* <div class="hero-image">
+            <figure>
+             <img src=" " alt="" width={300} />
+            </figure>
+          </div> */}
+        </div>
       </section>
       <section className='card-grid'>
         {users.map((user) => {
           return (
-            <Card key={user.id} user={user}/>
+            <Card key={user.id} user={user} />
           )
         })}
       </section>
