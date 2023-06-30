0ffcae28f9740ce8875aada9213b0d2582b789d2
santiago pasciullo <santiagopasciullo@gmail.com>
2023-06-30 20:38:34 -0300

    :art: Vistas mobile

--- src/Components/ErrorMsg.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrorMsg = () => {
-  return (
-    <div>ErrorMsg</div>
-  )
-}
-
-export default ErrorMsg
--- src/Components/SuccessMsg.jsx
+++ src/Components/SuccessMsg.jsx
@@ -5,8 +5,8 @@ import '../assets/styles/components/Messages.css'
 const SuccessMsg = ({ user }) => {
   return (
     <main className='success-msg-container'>
-      <h2>Gracias por contactarnos {user.name}</h2>
-      <p>Te responderemos a la brevedad a tu correo: {user.email}</p>
+      <h2>Gracias por contactarnos <strong>{user.name}</strong></h2>
+      <p>Te responderemos a la brevedad a tu correo: <strong>{user.email}</strong></p>
       <Link to='/' className='hero-btn' >Volver al inicio</Link>
     </main>
   )
